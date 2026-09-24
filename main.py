a3058974b6654509913b3011f3a10f75b2618480
Robertus Nugroho <robertusnugroho03@gmail.com>
2026-09-24 18:46:33 -0400

    hand landmarkers are getting read and printed

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2 as cv
 import mediapipe as mp 
 import numpy as np
+import os
+import time
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-import os
+
 
 # Get the directory where this current script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -15,7 +17,11 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+latest_result = None
+
 def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    global latest_result
+    latest_result = result
     print('hand marker result: {}'. format(result))
     
 options = HandLandmarkerOptions( 
@@ -24,22 +30,31 @@ options = HandLandmarkerOptions(
     result_callback = print_result,
     num_hands = 2,
     )
+
 with HandLandmarker.create_from_options(options) as landmarker:
-    print_result
     
-# Open camera with OpenCV
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("camera cannot be opened")
-    exit()
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("cannot recieve frame .. exiting")
-        break
-    cv.imshow('Live Stream', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+    # Open camera with OpenCV
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("camera cannot be opened")
+        exit()
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("cannot recieve frame .. exiting")
+            break
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+        timestamp_ms = int(time.time() * 1000)
+        landmarker.detect_async(mp_image, timestamp_ms)
+            
+        cv.imshow('Live Stream', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+    
+    
+
+
 
 cap.release()
 cv.destroyAllWindows()
