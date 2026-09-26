f1c6ad042160375aac522751e9b416809e0fa544
Robertus Nugroho <robertusnugroho03@gmail.com>
2026-09-25 22:35:05 -0400

    visualization tool implemented

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import mediapipe as mp
 import numpy as np
 import os
 import time
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+
 
 
 # Get the directory where this current script is located
@@ -17,11 +16,21 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+mp_hands = mp.tasks.vision.HandLandmarksConnections
+mp_drawing = mp.tasks.vision.drawing_utils
+mp_drawing_styles = mp.tasks.vision.drawing_styles
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+
 latest_result = None
 
 def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     global latest_result
     latest_result = result
+    print(latest_result)
     
     
 options = HandLandmarkerOptions( 
@@ -29,30 +38,46 @@ options = HandLandmarkerOptions(
     running_mode = VisionRunningMode.LIVE_STREAM,
     result_callback = print_result,
     num_hands = 2,
-    )
-
-with HandLandmarker.create_from_options(options) as landmarker:
+    min_hand_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+    min_hand_presence_confidence = 0.5)
     
+    
+
+
+
+with HandLandmarker.create_from_options(options) as hands:
     # Open camera with OpenCV
     cap = cv.VideoCapture(0)
     if not cap.isOpened():
         print("camera cannot be opened")
         exit()
+        
+        
     while True:
-        ret, frame = cap.read()
-        if not ret:
+        success, frame = cap.read()
+        if not success:
             print("cannot recieve frame .. exiting")
             break
         rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+        mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
         timestamp_ms = int(time.time() * 1000)
-        landmarker.detect_async(mp_image, timestamp_ms)
-            
-        cv.imshow('Live Stream', frame)
+        hands.detect_async(mp_frame, timestamp_ms)
+        
+        if latest_result is not None:
+            height, width, _ = frame.shape
+            for hand in latest_result.hand_landmarks:
+                for landmark in hand:
+                    
+                    x = int(landmark.x * width)
+                    y = int(landmark.y * height)
+                    cv.circle(frame, (x,y), 10, (0, 0, 255), -1)
+        
+        cv.imshow('Window Title', cv.flip(frame, 1))
         if cv.waitKey(1) == ord('q'):
             break
     
-    
+
 
 
 
